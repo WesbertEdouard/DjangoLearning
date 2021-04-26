b989159a7615a7d193a0fbef4151209b2a458d67
Wesbert <wesbert@justlearn.io>
2021-04-26 17:15:08 -0400

    First push

---
+++ accounts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+# Register Customer Model Table in Django Admin Portal
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)

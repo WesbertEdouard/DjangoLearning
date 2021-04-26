8c32200dc7e8c2df11b8d633758c41efaae6167e
Wesbert <wesbert@justlearn.io>
2021-04-26 18:01:42 -0400

    Added Orders and Tag models while setting up Many To Many relationship in database

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,5 +5,6 @@ from .models import *
 
 # Register Customer Model Table in Django Admin Portal
 admin.site.register(Customer)
+admin.site.register(Tag)
 admin.site.register(Product)
 admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,14 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    # This class level function returns the name of the Customer in the admin portal insteal of the default django ID ex. Customer(1)
+    def __str__(self):
+        return self.name
+
 # Simple Product model with dicitonary
 
 
@@ -35,8 +43,15 @@ class Product(models.Model):
     # By adding the chocies parameter in CharField we are able to define the values that populate the dropdoown for the variable in our application
     # We set choices = to the dictionary above
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    # Below we are creating a Many to Many relationship between the the tags model and the product model
+    # normally relationships are One to Many but we explicitly name this variable to a Many to Many relationship that way multiple tags can be associate with multiple products
+    tags = models.ManyToManyField(Tag)
+
+    # This class level function returns the name of the Customer in the admin portal insteal of the default django ID ex. Customer(1)
+    def __str__(self):
+        return self.name
 
 # Simple Order model w/ dicitionary
 # Refer to Product Model for class logic
@@ -48,7 +63,11 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    # customer =
-    # product =
+    # ForeignKey parameter from models creates a database connection between this Customer field and the Customer models table in the database, allowing us to have a dropdown select for all customers
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    # ForeignKey to auto populate product field in Orders model with all products within the database
+    # we add the on_delete parameter so that in the event that a customer/product is deleted from database then the order will not be deleted but the customer/product value will be set to NULL
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
